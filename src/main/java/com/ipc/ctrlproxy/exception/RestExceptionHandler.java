966b7ab4d11a218085fb7d4e994ee7788d42e40e
LaincoMini <jean-yves@info-pro-conseils.com>
2024-05-16 12:09:36 -0400

    Deployed on cloud with prod config

--- src/main/java/com/ipc/ctrlproxy/exception/RestExceptionHandler.java
+++ src/main/java/com/ipc/ctrlproxy/exception/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ipc.ctrlproxy.exception;
 
+import com.ipc.ctrlproxy.model.status.Message;
 import com.ipc.ctrlproxy.model.status.Status;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -9,19 +10,20 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Arrays;
+
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler
 {
-
-    //other exception handlers
-
-    @ExceptionHandler(CRTLException.class)
-    protected ResponseEntity<Status> handleEntityNotFound(CRTLException ex) {
-        return buildResponseEntity(ex.status);
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<Status> handleException(Exception e) {
+        return new ResponseEntity<>(Status.builder()
+                .status("2")
+                .messageHeader(Arrays.asList(
+                        Message.builder().type("E").message("StatusCode : " + HttpStatus.INTERNAL_SERVER_ERROR).build(),
+                        Message.builder().type("E").message("Body : " + e.getMessage()).build()))
+                .build(), HttpStatus.OK);
     }
 
-    private ResponseEntity<Status> buildResponseEntity(Status status) {
-        return new ResponseEntity<>(status, HttpStatus.OK);
-    }
 }
